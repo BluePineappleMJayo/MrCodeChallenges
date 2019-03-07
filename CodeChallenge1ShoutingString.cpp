d7e592d728aaddce89236398d1daaa74ead8c94b
MJayo <48307337+BluePineappleMJayo@users.noreply.github.com>
2019-03-06 23:54:37 -0600

    It better

--- CodeChallenge1ShoutingString.cpp
+++ CodeChallenge1ShoutingString.cpp
@@ -12,20 +12,40 @@ void display(string twlHour, string twlMinuet, string m);
 int main()
 {
 	string time,m,twlHour,twlMinuet;
+	char ans;
+do{
 	input(twlHour,twlMinuet,m,time);
 	convert(twlHour,m);
 	display(twlHour,twlMinuet,m);
+	cout<< "Want to do another? (Y/N): ";
+	cin >> ans;	
+	}while(ans=='y'|| ans=='Y');
+	cout << "Bye!";
 	return 0;
 }
 void input(string& twlHour, string& twlMinuet,string&m,string time)
 {
-	int i,x;
+	int i,x,t;
+	stringstream sx,st;
+do{
 	cout << "Enter your time in the formate XX:XXxm : ";
 	cin >> time;
 	size_t junk= time.find(":");
 	twlHour=time.substr(0,junk);
 	twlMinuet=time.substr(junk+1,2);
 	m=time.substr(junk+3);
+	sx.clear();
+	st.clear();
+	sx<<twlMinuet;
+	sx>>x;
+	st<<twlHour;
+	st>>t;
+	if(x>60||t>12)
+		cout << "Please Enter a valid time."<<endl;
+	else
+	break;
+	}while(1);
+
 }
 void convert(string& twlHour, string m)
 {
@@ -35,9 +55,9 @@ void convert(string& twlHour, string m)
 		{
 			ss<<twlHour;
 			ss>>y;
-			y=y+12;
 			ss.clear();
 			ss.str("");
+			y=y+12;
 			ss<<y;
 			twlHour=ss.str();
 		}
@@ -49,5 +69,5 @@ void convert(string& twlHour, string m)
 }
 void display(string twlHour,string twlMinuet, string m)
 {
-	cout << twlHour << ":" << twlMinuet;
+	cout << twlHour << ":" << twlMinuet<<endl;
 }
